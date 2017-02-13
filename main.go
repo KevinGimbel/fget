61e78258a0ab92c14e944c0ed49abb5cbd17b1c4
Kevin Gimbel <hallo@kevingimbel.com>
2017-02-13 08:29:11 +0100

    Add HTTP Header; Add gitignore

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ var Log colog.Colog
 
 func init() {
 	config := make(map[string]string)
-	config["LogFormat"] = "[%s](%s) - %s"
-	config["TimeFormat"] = "2006-Jan-2"
+	config["LogFormat"] = "[%s][%s] - %s"
+	config["TimeFormat"] = "2006-Jan-2 15:04:05"
 	Log = colog.NewColog(config)
 }
 
@@ -32,19 +32,19 @@ EXAMPLE:
 }
 
 func main() {
+	// not enough arguments
 	if len(os.Args) != 2 {
 		showhelp()
 		os.Exit(1)
 	}
 
-	httpClient := &http.Client{}
 	// match the file name
 	r := regexp.MustCompile(`[^\\/]+$`)
 	filename := r.FindStringSubmatch(os.Args[1])
 
+	// Create http client and prepare the request
+	httpClient := &http.Client{}
 	req, err := http.NewRequest("GET", os.Args[1], nil)
-
-	// response, err := http.Get(os.Args[1])
 
 	if err != nil {
 		log.Fatal(err)
@@ -59,10 +59,10 @@ func main() {
 
 	defer response.Body.Close()
 	img, _ := os.Create(filename[0])
-	b, err := io.Copy(img, response.Body)
+	size, err := io.Copy(img, response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(Log.Success(fmt.Sprintf("Downloaded file %s (%v bytes)", filename[0], b)))
+	fmt.Println(Log.Success(fmt.Sprintf("Downloaded file %s (%v bytes)", filename[0], size)))
 }
