f5e3093f65fd4a21f89f47dc7630e9cefe648776
Kevin Gimbel <hallo@kevingimbel.com>
2017-04-16 16:31:32 +0200

    Update Error Logging; Add Version String

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/kevingimbel/colog"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"regexp"
+
+	"github.com/kevingimbel/colog"
 )
 
 var Log colog.Colog
+var version = "1.0.0"
 
 func init() {
 	config := make(map[string]string)
@@ -22,13 +23,15 @@ func init() {
 func showhelp() {
 	fmt.Fprintf(os.Stderr, `USAGE: %[1]s <url>
 Fetch a file by URL and save it.
-	
+
 OPTIONS:
-url			-			HTTP or HTTPS URL to the file which should be downloaded
+url - HTTP or HTTPS URL to the file which should be downloaded
 
 EXAMPLE:
 %[1]s https://i.kevingimbel.me/fget/sample.png
-`, os.Args[0])
+
+Version: %s
+`, os.Args[0], version)
 }
 
 func main() {
@@ -47,21 +50,24 @@ func main() {
 	req, err := http.NewRequest("GET", os.Args[1], nil)
 
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(Log.Error(fmt.Sprintf("Request failed: %s", err)))
+		os.Exit(1)
 	}
 
 	req.Header.Set("User-Agent", "fget")
 	response, err := httpClient.Do(req)
 
 	if response.StatusCode > 400 {
-		log.Fatal("Unable to download file. Response: ", response.Status)
+		fmt.Println(Log.Error(fmt.Sprintf("Unable to download file. Response: %s", response.Status)))
+		os.Exit(2)
 	}
 
 	defer response.Body.Close()
 	img, _ := os.Create(filename[0])
 	size, err := io.Copy(img, response.Body)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(Log.Error(fmt.Sprintf("Unable to save file. %s", err)))
+		os.Exit(3)
 	}
 
 	fmt.Println(Log.Success(fmt.Sprintf("Downloaded file %s (%v bytes)", filename[0], size)))
